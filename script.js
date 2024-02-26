ec36292eabc07a2b7b1a326d72c474dc9a419d10
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-02-26 10:41:59 -0300

    Criação de DOM (Ex.8)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 alert("Olá, usuário!")
+
+let formulario = document.getElementById('forms')
+
+
+forms.addEventListener("submit", function (event) {
+
+    let nome = document.getElementById("nome").value;
+    let alterar_aqui = document.getElementById("Avancar_fulano");
+    alterar_aqui.innerText = nome + ", avance";
+    event.preventDefault();
+
+})
